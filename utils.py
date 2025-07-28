b78f45818a106d13e99983f67d7fea026dffcb35
zeroLR <ceyanglab@gmail.com>
2025-07-28 23:53:35 +0800

    refactor: Remove transform_response_content function and its tests

--- utils.py
+++
@@ -1,5 +0,0 @@
-def transform_response_content(content: str, chunk_size: int = 2000) -> list[str]:
-    """
-    將 content 依 chunk_size 分割為多個字串，回傳 list[str]
-    """
-    return [content[i : i + chunk_size] for i in range(0, len(content), chunk_size)]
